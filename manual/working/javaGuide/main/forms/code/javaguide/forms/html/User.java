ef8e21e61f4831d526495d288636171eacceee4f
Lawrence Kim <ultimate1352@gmail.com>
2015-06-21 16:48:27 +0900

    remove unused code file

--- manual/working/javaGuide/main/forms/code/javaguide/forms/html/User.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
- */
-package javaguide.forms.html;
-
-public class User {
-    public String email;
-    public String password;
-}
